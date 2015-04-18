529146116e9cdc4206a7fe5ecf36f01395f46f2a
TheShadow29 <ark.sadhu2904@gmail.com>
2015-04-19 03:52:25 +0530

    gets some points on the map
    didn't work as expected, needed few more points to get it running.

---
+++ OpenCV trial/imgprocessing.cpp
@@ -0,0 +1,34 @@
+#include "opencv2/imgproc.hpp"
+#include "opencv2/core.hpp"
+#include <iostream>
+using namespace std;
+using namespace cv;
+
+Mat img;
+
+Mat getRoute()
+{
+    //cvtColor(img,img,CV_BGR2HSV);
+
+    Mat lowerb(img.rows, img.cols, CV_8UC3, Scalar(200,0,0));
+    Mat upperb(img.rows, img,cols, CV_8UC3, Scalar(255,50,50));
+
+    Mat blu  = (img<upperb) & (img>lowerb);
+
+    return blu;
+}
+
+int main()
+{
+    img = imread("Capture.jpg");
+
+    namedWindow("Map");
+    imshow("Map", img);
+
+    Mat newimg = getRoute();
+
+    namedWindow("Route");
+    imshow("Route", newimg);
+
+    waitKey(0);
+}
